39126bb1c2842bd6dd8c2777b002830a0e3b3d9f
Naulan.CHRZ <67024770+NaulaN@users.noreply.github.com>
2022-10-12 16:41:29 +0200

    SQL query test for account roles

--- scripts/test.sql
+++ scripts/test.sql
@@ -1,1 +1,6 @@
 
+RAISE NOTICE '- Check who is admins';
+SELECT c.prenom, c.nom FROM public.compte c INNER JOIN public.role r ON c.idrole=r.idrole WHERE c.idrole=1;
+
+RAISE NOTICE '- Check who is users';
+SELECT c.prenom, c.nom FROM public.compte c INNER JOIN public.role r ON c.idrole=r.idrole WHERE c.idrole=2;
