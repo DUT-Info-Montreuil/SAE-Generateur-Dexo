dbc3ba68330a70b90426a1d656740c32d30c9047
Naulan.CHRZ <67024770+NaulaN@users.noreply.github.com>
2022-10-12 10:58:52 +0200

    Update create-db.sql

--- scripts/create-db.sql
+++ scripts/create-db.sql
@@ -1,1 +1,85 @@
+-- CREATE DATABASE
+CREATE DATABASE sae_ge;
 
+
+-- TABLE ROLE
+DROP TABLE sae_ge.role IF EXIST;
+CREATE TABLE sae_ge.role
+(
+  'idRole' SERIAL PRIMARY KEY,
+  'nom' TEXT
+);
+
+-- TABLE COMPTE
+DROP TABLE sae_ge.compte IF EXIST;
+CREATE TABLE sae_ge.compte 
+(
+  'idCompte' SERIAL PRIMARY KEY, 
+  'idRole' SERIAL,
+  'nom' TEXT,
+  'prenom' TEXT,
+  'login' TEXT,
+  'password' TEXT,
+  'email' TEXT,
+  
+  CONSTRAINT fk_role FOREIGN KEY(idrole) REFERENCES sae_ge.role(idrole)
+);
+
+-- TABLE PHOTO
+DROP TABLE sae_ge.photo IF EXIST;
+CREATE TABLE sae_ge.photo 
+(
+  'idPhoto' SERIAL PRIMARY KEY,
+  'idCompte' SERIAL,
+  'nom' TEXT,
+  'url' TEXT,
+  'partager' BOOLEAN,
+  
+  CONSTRAINT fk_compte FOREIGN KEY(idcompte) REFERENCES sae_ge.compte(idcompte)
+);
+
+-- TABLE TEMPLATES
+DROP TABLE sae_ge.templates IF EXIST;
+CREATE TABLE sae_ge.templates
+(
+  'idTemplate' SERIAL PRIMARY KEY,
+  'idCompte' SERIAL,
+  /* TODO: add other attr */
+  
+  CONSTRAINT fk_compte FOREIGN KEY(idcompte) REFERENCES sae_ge.compte(idcompte)
+);
+
+-- TABLE HISTORIQUE
+DROP TABLE sae_ge.historique IF EXIST;
+CREATE TABLE sae_ge.historique
+(
+  'idHistorique' SERIAL PRIMARY KEY,
+  'idCompte' SERIAL,
+  'idTemplate' SERIAL,
+  /* TODO: add other attr */
+  
+  CONSTRAINT fk_compte FOREIGN KEY(idcompte) REFERENCES sae_ge.compte(idcompte),
+  CONSTRAINT fk_template FOREIGN KEY(idtemplate) REFERENCES sae_ge.compte(idtemplate)
+);
+
+-- TABLE CATEGORIE
+DROP TABLE sae_ge.categorie IF EXIST;
+CREATE TABLE sae_ge.categorie
+(
+  'idCategorie' SERIAL PRIMARY KEY,
+  'nom' TEXT
+);
+
+-- TABLE EXERCICE
+DROP TABLE sae_ge.exercices IF EXIST;
+CREATE TABLE sae_ge.exercices
+(
+  'idExercice' SERIAL PRIMARY KEY,
+  'idCompte' SERIAL,
+  'idCategorie' SERIAL,
+  'nom' TEXT,
+  'data' TEXT,
+
+  CONSTRAINT fk_compte FOREIGN KEY(idcompte) REFERENCES sae_ge.compte(idcompte),
+  CONSTRAINT fk_categorie FOREIGN KEY(idcategorie) REFERENCES sae_ge.categorie(idcategorie)
+);
