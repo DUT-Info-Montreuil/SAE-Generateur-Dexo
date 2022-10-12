8fccc14bf5b2aa530ce77cac4e6cd909104b616a
Naulan.CHRZ <67024770+NaulaN@users.noreply.github.com>
2022-10-12 15:33:17 +0200

    Update create-db.sql

--- scripts/create-db.sql
+++ scripts/create-db.sql
@@ -1,106 +1,105 @@
 -- CREATE DATABASE
-CREATE DATABASE sae_ge
+/* CREATE DATABASE sae_ge
   WITH
   ENCODING = 'UTF8';
 COMMENT ON DATABASE sae_ge IS 'Base de donnée de la SAE sur le site du générateur dexercice';
 
-USE sae_ge;
+USE sae_ge; */
 
 
 -- TABLE ROLE
-DROP TABLE public.role IF EXIST;
+DROP TABLE IF EXISTS public.role;
 CREATE TABLE public.role
 (
-  'idRole' SERIAL PRIMARY KEY,
-  'nom' TEXT
+  idRole SERIAL PRIMARY KEY,
+  nom TEXT
 );
 
 -- TABLE COMPTE
-DROP TABLE public.compte IF EXIST;
+DROP TABLE IF EXISTS public.compte;
 CREATE TABLE public.compte 
 (
-  'idCompte' SERIAL PRIMARY KEY, 
-  'idRole' SERIAL,
-  'nom' TEXT,
-  'prenom' TEXT,
-  'login' TEXT,
-  'password' TEXT,
-  'email' TEXT,
+  idCompte SERIAL PRIMARY KEY, 
+  idRole SERIAL,
+  nom TEXT,
+  prenom TEXT,
+  login TEXT,
+  password TEXT,
+  email TEXT,
   
   CONSTRAINT fk_role FOREIGN KEY(idrole) REFERENCES public.role(idrole)
 );
 
 -- TABLE PHOTO
-DROP TABLE public.photo IF EXIST;
+DROP TABLE IF EXISTS public.photo;
 CREATE TABLE public.photo 
 (
-  'idPhoto' SERIAL PRIMARY KEY,
-  'idCompte' SERIAL,
-  'nom' TEXT,
-  'url' TEXT,
-  'partager' BOOLEAN,
+  idPhoto SERIAL PRIMARY KEY,
+  idCompte SERIAL,
+  nom TEXT,
+  url TEXT,
+  partager BOOLEAN,
   
   CONSTRAINT fk_compte FOREIGN KEY(idcompte) REFERENCES public.compte(idcompte)
 );
 
 -- TABLE CATEGORIE
-DROP TABLE public.categorie IF EXIST;
+DROP TABLE IF EXISTS public.categorie;
 CREATE TABLE public.categorie
 (
-  'idCategorie' SERIAL PRIMARY KEY,
-  'nom' TEXT
+  idCategorie SERIAL PRIMARY KEY,
+  nom TEXT
 );
 
 -- TABLE EXERCICE
-DROP TABLE public.exercices IF EXIST;
+DROP TABLE IF EXISTS public.exercices;
 CREATE TABLE public.exercices
 (
-  'idExercice' SERIAL PRIMARY KEY,
-  'idCompte' SERIAL,
-  'idCategorie' SERIAL,
-  'nom' TEXT,
-  'data' JSON,
+  idExercice SERIAL PRIMARY KEY,
+  idCompte SERIAL,
+  idCategorie SERIAL,
+  nom TEXT,
+  data JSON,
 
   CONSTRAINT fk_compte FOREIGN KEY(idcompte) REFERENCES public.compte(idcompte),
   CONSTRAINT fk_categorie FOREIGN KEY(idcategorie) REFERENCES public.categorie(idcategorie)
 );
 
 -- TABLE TEMPLATES
-DROP TABLE public.templates IF EXIST;
+DROP TABLE IF EXISTS public.templates;
 CREATE TABLE public.templates
 (
-  'idTemplate' SERIAL PRIMARY KEY,
-  'idCompte' SERIAL,
-  'idExercice' SERIAL,
-  'nom' TEXT,
-  'data' JSON,
+  idTemplate SERIAL PRIMARY KEY,
+  idCompte SERIAL,
+  idExercice SERIAL,
+  nom TEXT,
+  data JSON,
   
   CONSTRAINT fk_compte FOREIGN KEY(idcompte) REFERENCES public.compte(idcompte),
   CONSTRAINT fk_exercice FOREIGN KEY(idexercice) REFERENCES public.exercices(idexercice)
 );
 
 -- TABLE HISTORIQUE
-DROP TABLE public.historique IF EXIST;
+DROP TABLE IF EXISTS public.historique;
 CREATE TABLE public.historique
 (
-  'idHistorique' SERIAL PRIMARY KEY,
-  'idCompte' SERIAL,
-  'idTemplate' SERIAL,
-  'data' JSON,
+  idHistorique SERIAL PRIMARY KEY,
+  idCompte SERIAL,
+  idTemplate SERIAL,
+  data JSON,
   
   CONSTRAINT fk_compte FOREIGN KEY(idcompte) REFERENCES public.compte(idcompte),
-  CONSTRAINT fk_template FOREIGN KEY(idtemplate) REFERENCES public.compte(idtemplate)
+  CONSTRAINT fk_template FOREIGN KEY(idtemplate) REFERENCES public.templates(idtemplate)
 );
 
 -- RELATION HISTORIQUE COMPTE
-DROP TABLE public.templates_exercice IF EXIST;
+DROP TABLE IF EXISTS public.templates_exercice;
 CREATE TABLE public.templates_exercice
 (
-  'idTemplate' SERIAL,
-  'idExercice' SERIAL,
+  idTemplate SERIAL,
+  idExercice SERIAL,
   
   PRIMARY KEY(idtemplate, idexercice),
-  FOREIGN KEY(idtemplate) REFERENCES public.template(idtemplate),
+  FOREIGN KEY(idtemplate) REFERENCES public.templates(idtemplate),
   FOREIGN KEY(idexercice) REFERENCES public.exercices(idexercice)
 );
-
