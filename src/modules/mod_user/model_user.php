b679983702cb8e6e22b4938d533291b1a4097419
Tiago Narciso <tig2468.a@gmail.com>
2022-10-19 16:04:20 +0200

    remise en forme dossier module

---
+++ src/modules/mod_user/model_user.php
@@ -0,0 +1,28 @@
+<?php
+    require_once "./connexion.php";
+    
+    class ModelUser extends Connexion{
+        public function __construct(){
+        
+        }
+
+        public function login()
+        {
+            $username = $_POST['uname'];
+            $pass = $_POST['psw'];
+
+            $query ="SELECT username ,password FROM Compte WHERE login like ?";
+            $prep = parent::$bdd->prepare($query);
+            $prep->execute([$username]);
+
+            $login_info = $prep->fetch();
+
+            if (!is_null($login_info['password'])){
+                if (password_verify($pass,$login_info['password'])) {
+                    $_SESSION['id'] = $login_info['id'];
+                    $_SESSION['username'] = $id;
+                }
+            }
+        }
+    }
+?>
