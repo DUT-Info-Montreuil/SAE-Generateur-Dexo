7b35e4abf02e2957f0c60149a8e29575ed7dcf31
NaulaN <nonochrzaszcz@gmail.com>
2022-10-19 12:17:42 +0200

    postgresql connection done

---
+++ src/connexion.php
@@ -0,0 +1,20 @@
+<?php
+class Connexion
+{
+    static protected $bdd;
+
+
+    public function __construct() { }
+
+    public static function set_up_connection()
+    {
+        $file_name = "key.json";
+        $data = file_get_contents("../res/".$file_name);
+        $json = json_decode($data);
+
+        $host = "vps-db5011c7.vps.ovh.net";
+        $bdd_name = "sae_ge";
+        self::$bdd = new PDO("pgsql:host=$host;dbname=$bdd_name", $json->user, $json->password);
+    }
+}
+?>
