46b762e4f0871f45c28c996613474b9cd3ad83ff
CHRZASZCZ Naulan <nonochrzaszcz@gmail.com>
2022-10-23 00:22:00 +0200

    Correction code button

--- src/vue_user.js
+++ src/vue_user.js
@@ -1,11 +1,17 @@
 /* Script created at 22/10/2022 */
 
 const loginUrl = "./index.php?module=user&status=login";
+const registerUrl = "./index.php?module=user&status=register";
 const homeUrl = "./index.php?status=home";
 
 const connexionButton = document.getElementById("connexion-button");
+const registerButton = document.getElementById("register-button");
 const backButton = document.getElementById("back-button");
 
 
-connexionButton.onclick = function() { document.location = loginUrl; }
-backButton.onclick = function() { document.location = homeUrl; }
+if (registerButton != null)
+    registerButton.onclick = function() { document.location = registerUrl; }
+if (connexionButton != null)
+    connexionButton.onclick = function() { document.location = loginUrl; }
+if (backButton != null)
+    backButton.onclick = function() { document.location = homeUrl; }
