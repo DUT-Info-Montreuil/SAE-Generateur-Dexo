62972a1a85e67eb6fcb87dac82164481d6fee65a
almerion <tig2468.a@gmail.com>
2022-10-22 13:36:25 +0200

    adding style.css

--- src/index.php
+++ src/index.php
@@ -14,6 +14,7 @@ $content = $cont->getDisplay();
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="../res/style.css" rel="stylesheet">
         <title>Essaie</title>
     </head>
 
