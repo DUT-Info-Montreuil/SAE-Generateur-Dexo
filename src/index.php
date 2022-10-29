c88254f18370e705ba31f4367a68350bbb046c30
almerion <tig2468.a@gmail.com>
2022-10-29 12:14:37 +0200

    small fix doctype + css

--- src/index.php
+++ src/index.php
@@ -1,8 +1,6 @@
-<!DOCTYPE html>
-<html lang="fr">
-    <?php
+<?php
         ini_set('display_errors', 1);
-        
+
         require_once "./modules/mod_user/mod_user.php";
         require_once "./modules/mod_home/mod_home.php";
         require_once "./connexion.php";
@@ -23,18 +21,20 @@
             default :
                 break;
         }
-        $content = $module->getDisplay();
-    ?>
+        $content = $module->getDisplay(); ?>
 
+    <!DOCTYPE html>
+    <html lang="fr">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="../res/global.css" rel="stylesheet"/>
         <script src="css-loader.js" type="text/javascript"></script>
+        <link href="../res/home-page.css" rel="stylesheet"/>
         <title>SAE-GE</title>
     </head>
-        
+
     <body>
         <?= $content ?>
         <div id="popInErrorDiv"></div>
