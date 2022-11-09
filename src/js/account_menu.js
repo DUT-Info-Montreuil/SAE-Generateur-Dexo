6d5e794772ebbc007e00c308c789f67c06ff2ea9
NaulaN <nonochrzaszcz@gmail.com>
2022-11-09 10:23:18 +0100

    Write better js

--- src/js/account_menu.js
+++ src/js/account_menu.js
@@ -1,16 +1,21 @@
+/* Script created at 09/11/2022 */
 
 const accountButton = document.getElementById("account-button");
 const accountMenu = document.getElementsByClassName("account-menu");
+const measure = "px";
 
 let showMenu = false;
 
 
 accountButton.addEventListener("click", () => {
-    showMenu = !showMenu;
-    accountMenu[0].style.display = showMenu ? "flex" : "none";
+    const menu = accountMenu[0];
+    const button = accountButton;
 
-    let posAccountButton = accountButton.getBoundingClientRect();
-    accountMenu[0].style.setProperty("left", posAccountButton.left.toString() + "px");
-    accountMenu[0].style.setProperty("top", posAccountButton.top.toString() + "px");
+    showMenu = !showMenu;
+    menu.style.display = showMenu ? "flex" : "none";
+
+    const posButton = button.getBoundingClientRect();
+    menu.style.setProperty("left", posButton.left.toString() + measure);
+    menu.style.setProperty("top", posButton.top.toString() + measure);
 });
 
