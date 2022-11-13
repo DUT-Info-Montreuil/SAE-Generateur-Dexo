acdba0a1a56e4044961d74c4c89fcc431c3b865e
almerion <tig2468.a@gmail.com>
2022-11-13 14:12:49 +0100

    fix displacement problems + json output update auto

--- src/js/exercices.js
+++ src/js/exercices.js
@@ -2,6 +2,7 @@ let selectedItem = null;
 let draggedElement = null;
 let rawAllOptions;
 let mooveX, mooveY;
+let id = 0;
 fetch('../res/exerciceOptions.json')
     .then((res) => res.json())
     .then((json) => rawAllOptions = json);
@@ -11,8 +12,8 @@ const preview = document.getElementById('preview');
 const optionAside = document.getElementById('options');
 const page = {
     title: "",
-    elements: [],
-    height: 300
+    elements: Array(),
+    height: "5cm"
 };
 preview.addEventListener("mousedown", (ev) => {
     if (ev.target !== preview) {
@@ -24,35 +25,25 @@ preview.addEventListener("mousedown", (ev) => {
 })
 preview.addEventListener("mousemove", (ev) => {
     if (draggedElement != null) {
-        let elementHeight = window.getComputedStyle(draggedElement, null).getPropertyValue('height');
-        let elementWidth = window.getComputedStyle(draggedElement, null).getPropertyValue('width');
-        let bounce = preview.getBoundingClientRect();
-        let mousePosRelativelyToPreviewX = ev.clientX - bounce.left - mooveX;
-        let mousePosRelativelyToPreviewY = ev.clientY - bounce.top - mooveY;
+        let elementHeight = parseFloat(window.getComputedStyle(draggedElement, null).getPropertyValue('height').split('px')[0]);
+        let elementWidth = parseFloat(window.getComputedStyle(draggedElement, null).getPropertyValue('width').split('px')[0]);
+        const bound = preview.getBoundingClientRect();
+        const relativeMousePositions = getRelativePositionsMouse(bound, ev);
+        let mousePosRelativelyToPreviewX = relativeMousePositions.posX - mooveX;
+        let mousePosRelativelyToPreviewY = relativeMousePositions.posY - mooveY;
+
         if ((mousePosRelativelyToPreviewY > 0 && mousePosRelativelyToPreviewX > 0)
-            && (mousePosRelativelyToPreviewY < bounce.height && mousePosRelativelyToPreviewX < bounce.width)) {
+            && (mousePosRelativelyToPreviewY + elementHeight < bound.height && mousePosRelativelyToPreviewX + elementWidth < bound.width)) {
             draggedElement.style.top = mousePosRelativelyToPreviewY + 'px';
             draggedElement.style.left = mousePosRelativelyToPreviewX + 'px';
+            updateObject(ev.target);
         }
     }
 })
-preview.addEventListener("mouseup", (ev) => {
-    if (draggedElement != null) {
-        draggedElement.classList.remove('preview-elements-moving');
-        draggedElement = null;
-    }
-})
-
-function createImage() {
-    const img = document.createElement("div");
-    img.classList.add('img-input');
-    return img;
-}
-
-preview.addEventListener('click', (el) => {
+preview.addEventListener('click', (ev) => {
     clearOptionAside();
-    if (el.composedPath()[0] !== preview) {
-        displayOptions(el.composedPath()[0]);
+    if (ev.composedPath()[0] !== preview) {
+        displayOptions(ev.composedPath()[0]);
     } else {
         if (selectedItem != null) {
             let element;
@@ -62,20 +53,37 @@ preview.addEventListener('click', (el) => {
             } else if (selectedItem === 'img') {
                 element = createImage();
             }
+            element.setAttribute('value', id);
             element.style.position = 'absolute';
-            element.style.left = el.offsetX.toString() + 'px';
-            element.style.top = el.offsetY.toString() + 'px';
-            // addDraggable(element);
+            element.style.left = ev.offsetX.toString() + 'px';
+            element.style.top = ev.offsetY.toString() + 'px';
+
             preview.append(element);
             displayOptions(element);
+            updateObject(element);
+            id++;
         }
     }
 })
+preview.addEventListener("mouseup", () => {
+    if (draggedElement != null) {
+        draggedElement.classList.remove('preview-elements-moving');
+        draggedElement = null;
+    }
+})
+
 for (let draggable of draggables) {
-    draggable.addEventListener('click', (el) => {
-        selectedItem = el.target.getAttribute('value');
+    draggable.addEventListener('click', (ev) => {
+        selectedItem = ev.target.getAttribute('value');
     })
 }
+
+function createImage() {
+    const img = document.createElement("div");
+    img.classList.add('img-input');
+    return img;
+}
+
 
 function clearOptionAside() {
     for (let i = 0; i < optionAside.children.length; i++) {
@@ -116,19 +124,55 @@ function createOptions(parameters, element, styleName) {
         const min = parameters.min;
         const max = parameters.max;
         const inputSlider = document.createElement('input');
-        const actual = window.getComputedStyle(element, null).getPropertyValue(styleName);
-        element.style.setProperty(styleName, actual + 'px');
+        const rawActual = window.getComputedStyle(element, null).getPropertyValue(styleName);
+        // converting px to pt
+        const filteredActual = (3 / 4) * parseFloat(rawActual.slice(0, rawActual.indexOf('p')));
+        element.style.setProperty(styleName, filteredActual + 'pt');
 
         inputSlider.type = "range";
         inputSlider.min = min;
         inputSlider.max = max;
-        inputSlider.value = actual;
+        inputSlider.value = filteredActual;
         inputSlider.classList.add("slider");
 
-        inputSlider.addEventListener('input', (ev) => {
-            element.style.setProperty(styleName, inputSlider.value + 'px');
+        inputSlider.addEventListener('input', () => {
+            element.style.setProperty(styleName, inputSlider.value + 'pt');
+            updateObject(element)
         })
         container.append(inputSlider);
     }
     return container;
+}
+
+function getRelativePositionsMouse(bound, event) {
+    return {
+        "posX": (event.clientX - bound.left),
+        "posY": (event.clientY - bound.top)
+    };
+}
+
+function updateObject(element) {
+    let elementId = element.getAttribute('value');
+    if (typeof elementId === "string") {
+        let objectToUpdate = page.elements.find((el) => {
+            return el.id === elementId;
+        })
+        if (objectToUpdate === undefined) {
+
+            const type = element.tagName;
+            page.elements.push({
+                "id": elementId,
+                "type": type,
+                "properties": {}
+            })
+            objectToUpdate = page.elements[page.elements.length - 1];
+        }
+        let index = 1;
+        let currentStyle = element.style[index]
+        while (currentStyle !== undefined) {
+            objectToUpdate.properties[element.style[index]] = element.style[element.style[index]]
+            index++;
+            currentStyle = element.style[index];
+        }
+    }
 }
