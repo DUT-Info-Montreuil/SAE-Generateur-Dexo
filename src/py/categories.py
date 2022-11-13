a5dc69023a5e3fbdbde1ec9aee66ef94bc28c81b
Naulan.CHRZ <67024770+NaulaN@users.noreply.github.com>
2022-11-13 11:52:31 +0100

    Improve refresh function

--- src/py/categories.py
+++ src/py/categories.py
@@ -2,6 +2,7 @@
 import datetime
 import json
 from html.parser import HTMLParser
+from os.path import exists
 
 from const import *
 from exercise_element_generator import ExerciseElementGenerator
@@ -21,12 +22,15 @@ class Categories:
     def __init__(self, connection) -> None:
         self.connection = connection
 
+    def first_launch(self) -> None:
+        """ first_launch() -> None """
+        pass
+
     def refresh(self, payload: str) -> None:
         """ refresh(self, payload: str) -> None
                 It allows to append into a html file when the exercise recently added.
 
-        :param payload: json format output from postgresql trigger
-        :return: Nothing """
+        :param payload: json format output from postgresql trigger """
 
         json_data = json.loads(payload)
         id_exercise: str = None
@@ -38,32 +42,42 @@ class Categories:
 
         # if 'idexercise' getted on payload
         if id_exercise is not None:
-            name_exercise: str = get_first_row_sql(self.connection, Const.GET_NAME_EXO_QUERY, id_exercise)[0]
-            name_category: str = get_first_row_sql(self.connection, Const.GET_NAME_CAT_QUERY, id_exercise)[0]
+            name_exercise: str = None
+            result = get_first_row_sql(self.connection, Const.GET_NAME_EXO_QUERY, id_exercise)
+            if result is not None:
+                name_exercise: str = result[0]
+
+            name_category: str = None
+            result = get_first_row_sql(self.connection, Const.GET_NAME_CAT_QUERY, id_exercise)
+            if result is not None:
+                name_category: str = result[0]
 
             # if the name_exercise and name_exercise found on postgresql database
             if name_category and name_exercise is not None:
-                print(f"[{datetime.datetime.now()}] Creation de l'exercice -> {name_exercise} dans la catégorie -> {name_category} en cours...")
+                path_file_html = self.categories_html_path[name_category]
+                if exists(path_file_html):
+                    print(f"[{datetime.datetime.now()}] Creation de l'exercice -> {name_exercise} dans la catégorie -> {name_category} en cours...")
 
-                content_html: str
-                div = ExerciseElementGenerator.generate_div(name_category, name_exercise, id_exercise)
-                with open(self.categories_html_path[name_category], 'r') as html_file:
-                    content_html = html_file.read()
+                    content_html: str
+                    with open(path_file_html, 'r') as html_file:
+                        content_html = html_file.read()
 
-                with open(self.categories_html_path[name_category], 'w') as html_file:
-                    i = 0
-                    final_data_html = ""
-                    len_content_without_end_tags = (len(content_html)-1) - (len(Const.END_TAGS)-1)
-                    # Need to rewrite the content of file, because we need to delete end tags of 'body' and 'html'
-                    while i < len_content_without_end_tags:
-                        final_data_html = final_data_html + content_html[i]
-                        i += 1
+                    div = ExerciseElementGenerator.generate_div(name_category, name_exercise, id_exercise)
+                    with open(path_file_html, 'w') as html_file:
+                        i = 0; final_data_html = ""
+                        len_content_without_end_tags = (len(content_html)-1) - (len(Const.END_TAGS)-1)
+                        # Need to rewrite the content of file, because we need to delete end tags of 'body' and 'html'
+                        while i < len_content_without_end_tags:
+                            final_data_html = final_data_html + content_html[i]
+                            i += 1
 
-                    final_data_html = final_data_html + div + Const.END_TAGS
-                    html_file.write(final_data_html)
-                    print(f"[{datetime.datetime.now()}] L'exercice {name_exercise} a bien etais ecris dans {self.categories_html_path[name_category]}")
-                    print(f"[{datetime.datetime.now()}] Taille ddu fichier ecrit: {html_file.__sizeof__()}B")
-                # On success
-                return None
+                        final_data_html = final_data_html + div + Const.END_TAGS
+                        html_file.write(final_data_html)
+                        print(f"[{datetime.datetime.now()}] L'exercice {name_exercise} a bien etais ecris dans {path_file_html}")
+                        print(f"[{datetime.datetime.now()}] Taille ddu fichier ecrit: {html_file.__sizeof__()}B")
+                    # On success
+                    return None
+                else:
+                    print(f"[{datetime.datetime.now()}] Le fichier {path_file_html} n'existe pas")
         # On failure
         print(f"[{datetime.datetime.now()}] Le code ne peut continue, arrêt de la fonction")
