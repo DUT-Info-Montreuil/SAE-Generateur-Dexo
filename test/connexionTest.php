4ccbc002cf8d4c3529af9f82ee53cfb0b7618b8a
NChrzaszcz-Admin <nonochrzaszcz@gmail.com>
2022-11-24 17:05:52 +0100

    Refresh `connexionTest`

--- test/connexionTest.php
+++ test/connexionTest.php
@@ -1,17 +1,16 @@
 <?php
+require_once "../src/connexion.php";
+
 use PHPUnit\Framework\TestCase;
 
-
-class Module extends Connexion { public static function getBdd() { return self::$bdd; } }
 
 class ConnexionTest extends TestCase
 {
     public function test_set_up_connection()
     {
-        $bdd = Module::getBdd();
         Connexion::set_up_connection();
+        $bdd = Connexion::getBdd();
 
         $this->assertTrue(isset($bdd));
     }
 }
-?>
