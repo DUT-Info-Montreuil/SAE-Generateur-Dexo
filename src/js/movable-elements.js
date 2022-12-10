b829eb5058e42af1947977bdd300857946a295a4
CHRZASZCZ Naulan <nonochrzaszcz@gmail.com>
2022-12-10 16:35:58 +0100

    Position ajusted according to the scroll

--- src/js/movable-elements.js
+++ src/js/movable-elements.js
@@ -4,7 +4,6 @@ let clicked = false;
 let contentA4; let bound;
 A4.addEventListener("load", () => {
     contentA4 = A4.contentDocument.getElementById("exercises");
-    bound = contentA4.getBoundingClientRect();
 });
 
 function movableElementClickedEvent(event) {
@@ -14,7 +13,8 @@ function movableElementClickedEvent(event) {
 
 function followClickPointer(event) {
     if (clicked) {
-        movableElement.style.left = ((event.clientX - movableElement.width/2) - bound.left).toString() + "px";
-        movableElement.style.top = ((event.clientY - movableElement.height/2) - bound.top).toString() + "px";
+        bound = contentA4.getBoundingClientRect();
+        movableElement.style.left = (((event.clientX - movableElement.width/2) - bound.left) + contentA4.scrollLeft).toString() + "px";
+        movableElement.style.top = (((event.clientY - movableElement.height/2) - bound.top) + contentA4.scrollTop).toString() + "px";
     }
 }
