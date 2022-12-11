cab0559ee880c56c5379a839954639b6cf56a0f3
NaulaN <nonochrzaszcz@gmail.com>
2022-12-11 22:58:02 +0100

    Remove `openMenuImage` error

--- src/js/show_hide_popIn_Image.js
+++ src/js/show_hide_popIn_Image.js
@@ -1,21 +1,20 @@
 ///////////////////To show Image Menu (from vue_home.php)///////////////////////
 const iframeImgMenu = document.getElementById("pop-in_Image");
-const openMenuImage = document.getElementById("menuImg");
 
-openMenuImage.onclick = function () {
-    if (iframeImgMenu.style.display == 'block')
-        iframeImgMenu.style.display = 'none';
-    else
-        iframeImgMenu.style.display = 'block';
-};
 
 iframeImgMenu.addEventListener('load', function () {
+    const openMenuImage = document.getElementById("menuImg");
     const exitMenu = iframeImgMenu.contentDocument.getElementById("exit");
 
     exitMenu.onclick = function () {
-        if (iframeImgMenu.style.display == 'block')
+        if (iframeImgMenu.style.display === 'block')
             iframeImgMenu.style.display = 'none';
-        else
-            iframeImgMenu.style.display = 'block';
+        else iframeImgMenu.style.display = 'block';
     }
+
+    openMenuImage.onclick = function () {
+        if (iframeImgMenu.style.display === 'block')
+            iframeImgMenu.style.display = 'none';
+        else iframeImgMenu.style.display = 'block';
+    };
 });
