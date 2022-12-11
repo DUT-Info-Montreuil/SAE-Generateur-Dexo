a9b6d6302890a8fe561468f6392f84b7425d90e3
NaulaN <nonochrzaszcz@gmail.com>
2022-12-11 23:28:17 +0100

    Edit exo drag and drop

--- src/ajax/get_exercise_content.php
+++ src/ajax/get_exercise_content.php
@@ -6,7 +6,7 @@ Connexion::set_up_connection("../../res/");
 
 if (isset($_POST['id_exo'])) {
     $id_exo = $_POST['id_exo'];
-    $query = "SELECT data FROM exercices INNER JOIN compte on (exercices.idcompte = compte.idcompte) where idexercice = :id_ex  "; // AND idrole = 2;
+    $query = "SELECT data FROM exercices INNER JOIN compte on (exercices.idcompte = compte.idcompte) where idexercice = :id_ex AND idrole = 1;";
     $prepare = Connexion::getBdd()->prepare($query);
     $prepare->bindValue(':id_ex', $id_exo, PDO::PARAM_INT);
     try {
