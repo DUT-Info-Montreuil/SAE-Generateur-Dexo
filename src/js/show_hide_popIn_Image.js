58a5b0f1ef861cb1ac0bf0cd30ec1725e92562da
NaulaN <nonochrzaszcz@gmail.com>
2022-12-11 23:02:10 +0100

    Remove useless script into `upload-Image.html`

--- src/js/show_hide_popIn_Image.js
+++ src/js/show_hide_popIn_Image.js
@@ -1,18 +1,20 @@
 ///////////////////To show Image Menu (from vue_home.php)///////////////////////
-window.addEventListener('load', function () {
-    const iframeImgMenu = document.getElementById("pop-in_Image");
+const iframeImgMenu = document.getElementById("pop-in_Image");
+
+
+iframeImgMenu.addEventListener('load', function () {
     const openMenuImage = document.getElementById("menuImg");
     const exitMenu = iframeImgMenu.contentDocument.getElementById("exit");
 
     exitMenu.onclick = function () {
-        if (iframeImgMenu.style.display === 'block')
+        if (iframeImgMenu.style.display == 'block')
             iframeImgMenu.style.display = 'none';
         else iframeImgMenu.style.display = 'block';
     }
 
     openMenuImage.onclick = function () {
-        if (iframeImgMenu.style.display === 'block')
+        if (iframeImgMenu.style.display == 'block')
             iframeImgMenu.style.display = 'none';
         else iframeImgMenu.style.display = 'block';
-    };
+    }
 });
