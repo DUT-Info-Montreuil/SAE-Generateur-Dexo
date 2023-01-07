0b86edeb445bd9585950adff88b56f119ca1ad2b
CHRZASZCZ Naulan <nonochrzaszcz@gmail.com>
2023-01-07 22:03:39 +0100

    Add license-part and credit-part

--- src/js/settings.js
+++ src/js/settings.js
@@ -5,8 +5,13 @@ settings.addEventListener("load", () => {
     const cancelButton = settings.contentDocument.getElementsByClassName("cancel-button");
     const displayButton = settings.contentDocument.getElementById("display-button");
     const accountButton = settings.contentDocument.getElementById("account-button");
+    const licenseButton = settings.contentDocument.getElementById("license-button");
+    const creditButton = settings.contentDocument.getElementById("credit-button");
+
     const contentDisplay = settings.contentDocument.getElementById("display-part");
     const contentAccount = settings.contentDocument.getElementById("account-part");
+    const contentLicense = settings.contentDocument.getElementById("license-part");
+    const contentCredit = settings.contentDocument.getElementById("credit-part");
 
     for (let butt of cancelButton)
         butt.addEventListener("click", () => {
@@ -17,10 +22,28 @@ settings.addEventListener("load", () => {
     displayButton.addEventListener("click", () => {
         contentDisplay.style.display = "block";
         contentAccount.style.display = "none";
+        contentLicense.style.display = "none";
+        contentCredit.style.display = "none";
     });
 
     accountButton.addEventListener("click", () => {
-        contentAccount.style.display = "block";
         contentDisplay.style.display = "none";
+        contentAccount.style.display = "block";
+        contentLicense.style.display = "none";
+        contentCredit.style.display = "none";
+    });
+
+    licenseButton.addEventListener("click", () => {
+        contentDisplay.style.display = "none";
+        contentAccount.style.display = "none";
+        contentLicense.style.display = "block";
+        contentCredit.style.display = "none";
+    });
+
+    creditButton.addEventListener("click", () => {
+        contentDisplay.style.display = "none";
+        contentAccount.style.display = "none";
+        contentLicense.style.display = "none";
+        contentCredit.style.display = "block";
     });
 });
