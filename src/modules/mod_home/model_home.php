a2531667c30a862db34598ca6a8635f852dfcb05
Naulan.CHRZ <67024770+NaulaN@users.noreply.github.com>
2023-01-18 11:15:34 +0100

    Fix wrong name

--- src/modules/mod_home/model_home.php
+++ src/modules/mod_home/model_home.php
@@ -9,7 +9,7 @@ class ModelHome extends Connexion
     public function fetchImages($idcompte = null, $shared = null)
     {
         if ($idcompte == null || $shared == null)
-            $query = "SELECT * FROM photo p INNER JOIN compte c ON p.idcompte=c.idcompte WHERE (c.idrole = '1') OR (p.partager = 't')";
+            $query = "SELECT p.idphoto, p.idcompte, p.nom, p.partager, p.bin FROM photo p INNER JOIN compte c ON p.idcompte=c.idcompte WHERE (c.idrole = '1') OR (p.partager = 't')";
         else $query = "SELECT * FROM photo WHERE (partager = 'f') AND (idcompte = '".$idcompte."')";
         $list_images = array();
 
